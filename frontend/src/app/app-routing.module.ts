1178fe8f753abcc282a467585027069d305ad13c
satyam00043 <satyam00043@gmail.com>
2023-10-21 02:11:59 +0530

    edit3

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './component/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component:HeaderComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/component/header/header.component.ts
+++ frontend/src/app/component/header/header.component.ts
@@ -1,3 +1,4 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  public name="satyam"
+  public myid="satyam"
+  public greetings="";
+  public onClick(){
+    console.log("hellow  satyam  kumar");
+    this.greetings="satyam aap jinda hain bhosdike";
+  }
+  public Message(value: any){
+    console.log(value);
+
+  }
+  public text_success={
+    color:"green",
+  fontstyle:"italaic"  }
+
+  
+  public hasError=true;
+  public isSpecial=true;
+ 
 
 }
